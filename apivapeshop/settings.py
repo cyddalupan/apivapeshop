bc3ed236f375125b4293bde71a9b95013f99b4ed
Cyd Dalupan <cydmdalupan@gmail.com>
2023-07-29 07:48:13 +0000

    update config

--- apivapeshop/settings.py
+++ apivapeshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+		'corsheaders',
     'inventory',
     # 'inventory.apps.InventoryConfig',
 ]
@@ -50,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'apivapeshop.urls'
@@ -137,3 +139,5 @@ REST_FRAMEWORK = {
 STATIC_URL = '/staticfiles/' 
 STATIC_ROOT = str(BASE_DIR.joinpath('staticfiles'))
 STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
+CORS_ALLOW_ALL_ORIGINS = True
